03cdbdbddd727f5206a5206769ac29886af2bff9
Bryce Simmerman <brycesimmerman@gmail.com>
2023-11-21 23:10:51 -0500

    Set Up Western Conference Team Roster Call and View

--- Controllers/StatsController.cs
+++ Controllers/StatsController.cs
@@ -1,6 +1,12 @@
 using BardownskiBro.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using System.Diagnostics;
+using BardownskiBro.HelperClasses;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using Newtonsoft.Json;
+using System;
 
 namespace BardownskiBro.Controllers
 {
@@ -8,11 +14,15 @@ namespace BardownskiBro.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private IConfiguration _configuration;
+        protected string? BaseURL;
+        protected string? BaseURL2;
 
         public StatsController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
             _configuration = configuration;
+            BaseURL = _configuration is not null ? _configuration.GetValue<string>("NHLAPI:BaseURL") : "";
+            BaseURL2 = _configuration is not null ? _configuration.GetValue<string>("NHLAPI:BaseURL2") : "";
         }
 
         public IActionResult Index()
@@ -23,7 +33,77 @@ namespace BardownskiBro.Controllers
 
         public IActionResult TeamStats(string TeamName)
         {
-            return View();
+            if (TeamName.Length == 3)
+            {
+                try
+                {
+                    HttpClient httpClient = new HttpClient();
+                    string? response = httpClient.GetStringAsync(BaseURL + @$"roster/{TeamName}/current").Result;
+                    if (response is not null)
+                    {
+                        JObject? content = JSONHelper.StringToJObject(response);
+                        if (content is not null)
+                        {
+                            //PositionsHelper positions = (PositionsHelper)JsonConvert.DeserializeObject(response, typeof(PositionsHelper));
+                            string? forwardsOnlyString = content["forwards"].ToString();
+                            JArray? forwards = JArray.Parse(forwardsOnlyString);
+                            //Player positions = (Player)JsonConvert.DeserializeObject(response, typeof(PositionsHelper));
+                            //JArray? forwards = JArray.Parse(content["forwards"].ToString());
+                            //List<Player> forwardsList = forwards.ToObject<List<Player>>();
+                            List<Player> forwardsList = new List<Player>();
+                            //forwards.ToObject<Player>();
+                            //foreach (JToken forward in forwards)
+                            //{
+                            //    //(Player)JsonConvert.DeserializeObject
+                            //}
+                            foreach (JObject forward in forwards)
+                            {
+
+                                Player player = new Player()
+                                {
+                                    id = Int32.TryParse(forward.GetValue("id").ToString(), out int didItWork) == true ? didItWork : 0,
+                                    headshot = forward.GetValue("headshot").ToString(),
+                                    firstName = forward.GetValue("firstName")["default"].ToString(),
+                                    lastName = forward.GetValue("lastName")["default"].ToString(),
+                                    sweaterNumber = forward.GetValue("sweaterNumber").ToString(),
+                                    position = "F",
+                                    positionCode = forward.GetValue("positionCode").ToString(),
+                                    shootsCatches = forward.GetValue("shootsCatches").ToString(),
+                                    heightInInches = forward.GetValue("heightInInches").ToString(),
+                                    weightInPounds = forward.GetValue("weightInPounds").ToString(),
+                                    birthDate = forward.GetValue("birthDate").ToString(),
+                                    birthCity = forward.GetValue("birthCity")["default"].ToString(),
+                                    birthStateProvince = forward.GetValue("birthStateProvince") is not null ? forward.GetValue("birthStateProvince")["default"].ToString() : "",
+                                    birthCountry = forward.GetValue("birthCountry").ToString()
+                                };
+                                player.age = ConvertBDayToAge.ConvertDateStringToAge(player.birthDate);
+                                forwardsList.Add(player);
+                            }
+                            return View(forwardsList);
+                        }
+
+                    }
+                    else
+                    {
+                        return BadRequest("NHL API Returned A Null Response Result");
+                    }
+                }catch (Exception ex)
+                {
+                    if (_configuration.GetValue<string>("Environment") == "DEV")
+                    {
+                        return BadRequest(ex.Message + "\n" + ex.StackTrace + "\n" + ex.Source);
+                    }
+                    else
+                    {
+                        return BadRequest(ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                return BadRequest("Invalid Team Name");
+            }
+            return View(new List<Player>());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ HelperClasses/ConvertBDayToAge.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+namespace BardownskiBro.HelperClasses
+{
+    public static class ConvertBDayToAge
+    {
+        public static int ConvertDateStringToAge(string BDay)
+        {
+            DateTime now = DateTime.Now;
+            int age = 0;
+            if (BDay != null)
+            {
+                try
+                {
+                    DateTime Bday = DateTime.Parse(BDay);
+                    age = now.Year - Bday.Year;
+
+                    if (Bday.AddYears(age) > now)
+                    {
+                        age--;
+                    }
+
+                    return age;
+                }
+                catch (Exception e)
+                {
+                    return -1;
+                }
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
